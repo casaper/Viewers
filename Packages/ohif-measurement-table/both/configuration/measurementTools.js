dc8db530a32cd472a1bd3506b19ae9851d382d25
Gustavo Lelis <galelis@gmail.com>
2018-05-24 18:56:25 -0300

    Adding RectangleRoi into tracked tools

--- Packages/ohif-measurement-table/both/configuration/measurementTools.js
+++ Packages/ohif-measurement-table/both/configuration/measurementTools.js
@@ -1,10 +1,12 @@
 import { ToolGroupBaseSchema } from '../schema/tools/toolGroupSchema';
 import { length } from './tools/length';
 import { ellipticalRoi } from './tools/ellipticalRoi';
+import { rectangleRoi } from './tools/rectangleRoi';
 
 const trackedTools = [
     length,
-    ellipticalRoi
+    ellipticalRoi,
+    rectangleRoi
 ];
 
 export const measurementTools = [{
