eea52ecffd40714fc4a5fdf3173e885d1f74d35e
Kaspar Vollenweider <vok@panter.ch>
2018-06-19 17:52:33 +0200

    finish the setup so travis cy can run cypress e2e tests
    fix absent cypress binary
    
    find out if pwd is wrong in script part
    
    try paths in env vars and cypress install
    
    fix path env variables
    
    see if env vars work like this
    
    try cacheing cypress cache
    
    try fixing: ensure meteor allready runs when cypress trys to access it
    
    try more ensurement to have meteor allready reachable
    
    try to have meteor running before cypress with a bash script

--- LesionTracker/.cypress/integration/display_dicom.js
+++ LesionTracker/.cypress/integration/display_dicom.js
@@ -1,7 +1,7 @@
 describe('view the first images', () => {
 
   it('displays the ct', () => {
-    cy.visit('http://localhost:3000')
+    cy.visit('http://127.0.0.1:3000')
     expect(true).to.equal(true)
     cy.get('#btnTestDrive').click()
     cy.get('#studyDate').clear().type('06/19/1999 - 06/19/2018')
