5a35ad037c42f1fd6bff44a39a759609c178fc9e
Kaspar Vollenweider <vok@panter.ch>
2018-06-22 17:14:26 +0200

    add commands to use orthanc rest api

--- LesionTracker/.cypress/support/orthanc.js
+++ LesionTracker/.cypress/support/orthanc.js
@@ -1,7 +1,32 @@
 const orthancUrl = 'http://orthanc:orthanc@127.0.0.1:8042'
 
-Cypress.Commands.add('orthancPatients', () => cy.request('GET', `${orthancUrl}/patients`).its('body'))
-Cypress.Commands.add('orthancStudies', () => cy.request('GET', `${orthancUrl}/studies`).its('body'))
-Cypress.Commands.add('orthancSeries', () => cy.request('GET', `${orthancUrl}/series`).its('body'))
-Cypress.Commands.add('orthancInstances', () => cy.request('GET', `${orthancUrl}/instances`).its('body'))
+const getPatients = () => cy.request('GET', `${orthancUrl}/patients`).its('body')
+const getStudies = () => cy.request('GET', `${orthancUrl}/studies`).its('body')
+const getSeries = () => cy.request('GET', `${orthancUrl}/series`).its('body')
+const getInstances = () => cy.request('GET', `${orthancUrl}/instances`).its('body')
 
+const deletePatient = (patientId) => cy.request('DELETE', `${orthancUrl}/patients/${patientId}`)
+const deleteStudy = (studyId) => cy.request('DELETE', `${orthancUrl}/studies/${studyId}`)
+const deleteSerie = (serieId) => cy.request('DELETE', `${orthancUrl}/series/${serieId}`)
+const deleteInstance = (instanceId) => cy.request('DELETE', `${orthancUrl}/instances/${instanceId}`)
+
+// Get Requests to orthanc server API
+Cypress.Commands.add('orthancPatients', getPatients)
+Cypress.Commands.add('orthancStudies', getStudies)
+Cypress.Commands.add('orthancSeries', getSeries)
+Cypress.Commands.add('orthancInstances', getInstances)
+
+// DELETE via orthanc server API
+Cypress.Commands.add('orthancDeletePatient', patientId => deletePatient(patientId))
+Cypress.Commands.add('orthancDeletePatients', () => {
+  getPatients().then(patientIds => {
+    patientIds.forEach(patientId => deletePatient(patientId));
+  })
+})
+
+Cypress.Commands.add('orthancDeleteStudy', patientId => deletePatient(patientId))
+Cypress.Commands.add('orthancDeleteStudies', () => {
+  getPatients().then(patientIds => {
+    patientIds.forEach(patientId => deletePatient(patientId));
+  })
+})
