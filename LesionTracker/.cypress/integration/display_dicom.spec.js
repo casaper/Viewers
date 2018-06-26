d0f654301d92f3749371bbbd28a212a36b8dc1de
Kaspar Vollenweider <vok@panter.ch>
2018-06-26 20:18:50 +0200

    verify correct number of images have been loaded

--- LesionTracker/.cypress/integration/display_dicom.spec.js
+++ LesionTracker/.cypress/integration/display_dicom.spec.js
@@ -10,17 +10,16 @@ describe('display dicom image', () => {
           seriesInstances.forEach(({ FileUuid, FileSize, IndexInSeries, MainDicomTags: { SOPInstanceUID } }) => {
             cy.route({ method: 'GET', url: `**objectUID=${SOPInstanceUID}**`,
               onResponse: xhr => {
-                console.log(xhr)
                 expect(xhr.response.headers).to.have.property('content-type', 'application/dicom')
+                expect(xhr.response.body.byteLength).to.eq(FileSize)
               }
             }).as(FileUuid)
           })
 
-
+          // select serie to view
           cy.get('input[name="daterange"]').clear().type('06/22/1999 - 06/22/2018')
           cy.get('button').contains('Apply').click()
           cy.get('body').contains('Jan 10, 2018').dblclick()
-
 
           // Check out some different image indexes
           cy.get('.topright').should('contain', 'LUNG').should('contain', '1/68')
@@ -38,7 +37,10 @@ describe('display dicom image', () => {
           cy.get('div.topleft.dicomTag').should('contain', 'R00000004').should('contain', 'Example Patient')
 
           // Wait for all images to be loaded
-          cy.wait(seriesInstances.map(({ FileUuid }) => `@${FileUuid}`), { timeout: 30000 })
+          cy.wait(seriesInstances.map(({ FileUuid }) => `@${FileUuid}`), { timeout: 30000 }).then(wadoXHRRequests => {
+            // verify if correct ammount of images has been requested and received
+            expect(wadoXHRRequests).to.have.length(seriesInstances.length)
+          })
         })
       })
     })
