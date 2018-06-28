88eef47abf9f1d78aaa712e706b7c3adec5f2460
Kaspar Vollenweider <vok@panter.ch>
2018-06-28 15:27:59 +0200

    revert desperacy tryouts

--- LesionTracker/.cypress/integration/display_dicom.spec.js
+++ LesionTracker/.cypress/integration/display_dicom.spec.js
@@ -1,47 +1,47 @@
 describe('display dicom image', () => {
   it('shows dicom image', async () => {
-    cy.reloadOrthancServerImages().then(() => {
-      await cy.mongoRestore('../test/db_snapshots/01_initial_with_testing_user.gz').promisify()
+    await cy.reloadOrthancServerImages().promisify()
+    await cy.mongoRestore('../test/db_snapshots/01_initial_with_testing_user.gz').promisify()
 
-      // Get all the instances from one dates serie
-      const seriesInstances = await cy.getSerieInstanceUids('20180110').promisify()
-      await cy.login()
+    // Get all the instances from one dates serie
+    const seriesInstances = await cy.getSerieInstanceUids('20180110').promisify()
+    await cy.login()
 
-      // Setup route listeners for all images in serie
-      seriesInstances.forEach(({ FileUuid, FileSize, IndexInSeries, MainDicomTags: { SOPInstanceUID } }) => {
-        cy.route({ method: 'GET', url: `**objectUID=${SOPInstanceUID}**`,
-          onResponse: xhr => {
-            expect(xhr.response.headers).to.have.property('content-type', 'application/dicom')
-            expect(xhr.response.body.byteLength).to.eq(FileSize)
-          }
-        }).as(FileUuid)
-      })
-
-      // select serie to view
-      cy.get('input[name="daterange"]').clear().type('06/22/1999 - 06/22/2018')
-      cy.get('button').contains('Apply').click()
-      cy.get('body').contains('Jan 10, 2018').dblclick()
-
-      // Check out some different image indexes
-      cy.get('.topright').should('contain', 'LUNG').should('contain', '1/68')
-      cy.get('.imageSlider')
-          .invoke('val', 10).trigger('change')
-        .get('.topright')
-          .should('contain', '10/68')
-      cy.get('.imageSlider')
-          .invoke('val', 68).trigger('change')
-        .get('.topright')
-          .should('contain', '68/68')
-
-
-      cy.get('div.bottomleft.dicomTag').should('contain', 'W: 1225, L: -670')
-      cy.get('div.topleft.dicomTag').should('contain', 'R00000004').should('contain', 'Example Patient')
-
-      // Wait for all images to be loaded
-      cy.wait(seriesInstances.map(({ FileUuid }) => `@${FileUuid}`), { timeout: 30000 }).then(wadoXHRRequests => {
-        // verify if correct ammount of images has been requested and received
-        expect(wadoXHRRequests).to.have.length(seriesInstances.length)
-      })
+    // Setup route listeners for all images in serie
+    seriesInstances.forEach(({ FileUuid, FileSize, IndexInSeries, MainDicomTags: { SOPInstanceUID } }) => {
+      cy.route({ method: 'GET', url: `**objectUID=${SOPInstanceUID}**`,
+        onResponse: xhr => {
+          expect(xhr.response.headers).to.have.property('content-type', 'application/dicom')
+          expect(xhr.response.body.byteLength).to.eq(FileSize)
+        }
+      }).as(FileUuid)
     })
+
+    // select serie to view
+    cy.get('input[name="daterange"]').clear().type('06/22/1999 - 06/22/2018')
+    cy.get('button').contains('Apply').click()
+    cy.get('body').contains('Jan 10, 2018').dblclick()
+
+    // Check out some different image indexes
+    cy.get('.topright').should('contain', 'LUNG').should('contain', '1/68')
+    cy.get('.imageSlider')
+        .invoke('val', 10).trigger('change')
+      .get('.topright')
+        .should('contain', '10/68')
+    cy.get('.imageSlider')
+        .invoke('val', 68).trigger('change')
+      .get('.topright')
+        .should('contain', '68/68')
+
+
+    cy.get('div.bottomleft.dicomTag').should('contain', 'W: 1225, L: -670')
+    cy.get('div.topleft.dicomTag').should('contain', 'R00000004').should('contain', 'Example Patient')
+
+    // Wait for all images to be loaded
+    cy.wait(seriesInstances.map(({ FileUuid }) => `@${FileUuid}`), { timeout: 30000 }).then(wadoXHRRequests => {
+      // verify if correct ammount of images has been requested and received
+      expect(wadoXHRRequests).to.have.length(seriesInstances.length)
+    })
+
   })
 })
